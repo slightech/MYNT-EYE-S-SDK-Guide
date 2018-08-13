592ef24d04e5e6ba138d1287a3c342f400ff726a
John Zhao <johnzhao@slightech.com>
2018-08-13 12:43:24 +0800

    Update version

--- conf.py
+++ conf.py
@@ -26,7 +26,7 @@ author = 'JohnZhao'
 # The short X.Y version
 version = '2.0.1'
 # The full version, including alpha/beta/rc tags
-release = '{}-rc0'.format(version)
+release = '{}-rc1'.format(version)
 
 
 # -- General configuration ---------------------------------------------------
